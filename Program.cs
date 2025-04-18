b18e26a9753f72c7f02b8deb6a2885de48cdf218
ST10457455 <162103646+ST10457455@users.noreply.github.com>
2025-04-18 12:28:38 +0200

    Added ASCII Art

--- Program.cs
+++ Program.cs
@@ -18,4 +18,24 @@ class Program
         Console.WriteLine("Welcome to the Cybersecurity Awareness Bot!");
         // Continue with the rest of your chatbot...
     }
+
+    static void DisplayAsciiArt()
+    {
+        Console.ForegroundColor = ConsoleColor.Cyan;
+        Console.WriteLine(@"
+
+
+       ______      __              _____                      _ __           ___                                                   ____        __ 
+      / ____/_  __/ /_  ___  _____/ ___/___  _______  _______(_) /___  __   /   |_      ______ _________  ____  ___  __________   / __ )____  / /_
+     / /   / / / / __ \/ _ \/ ___/\__ \/ _ \/ ___/ / / / ___/ / __/ / / /  / /| | | /| / / __ `/ ___/ _ \/ __ \/ _ \/ ___/ ___/  / __  / __ \/ __/
+    / /___/ /_/ / /_/ /  __/ /   ___/ /  __/ /__/ /_/ / /  / / /_/ /_/ /  / ___ | |/ |/ / /_/ / /  /  __/ / / /  __(__  |__  )  / /_/ / /_/ / /_  
+    \____/\__, /_.___/\___/_/   /____/\___/\___/\__,_/_/  /_/\__/\__, /  /_/  |_|__/|__/\__,_/_/   \___/_/ /_/\___/____/____/  /_____/\____/\__/  
+         /____/                                                 /____/                                                                           
+
+
+            Cybersecurity Awareness Bot        
+        ");
+        Console.ResetColor();
+    }
 }
+
